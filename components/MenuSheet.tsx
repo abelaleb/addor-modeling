28b64544085dd1fe8353a4f5e182ce783c1b3884
Abel Alebachew <abelalebachewasfaw@gmail.com>
2025-11-14 15:42:21 +0300

    fix: update old packages

--- components/MenuSheet.tsx
+++ components/MenuSheet.tsx
@@ -12,14 +12,10 @@ import {
 } from "./ui/sheet";
 import { Menu } from "lucide-react";
 import { useState } from "react";
-import { createBrowserClient } from "@supabase/ssr";
+
 const MenuSheet = () => {
   const [open,setOpen] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
@@ -28,15 +24,15 @@ const MenuSheet = () => {
       <SheetContent side="bottom" className="h-full">
         <SheetClose />
         <SheetHeader className="h-full">
-          {/* <SheetTitle>huh</SheetTitle> */}
+          <SheetTitle>ADDOR</SheetTitle>
           <SheetDescription className="h-full">
             <div className="h-full flex justify-center text-lg items-center flex-col gap-y-5">
               {publicMenus.map((menu: MenuProps, index: number) => {
                 return (
                   <div key={index}>
                     <Link href={menu.path} onClick={()=> setOpen(false)}>{menu.name.toUpperCase()}</Link>
-                  </div>
-                );
+                   </div>
+                )
               })}
             </div>
           </SheetDescription>
